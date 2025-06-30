99615e0924b33d82d468fb5cf7aac2fad8967ba6
İlayda Bakırcıoğlu <119436349+ilaydabkrc@users.noreply.github.com>
2025-06-30 23:37:04 +0300

    Add files via upload

---
+++ Linq/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+        List<int> sayilar = new List<int>();
+
+        for (int i= 0; i < 10; i++)
+        {
+            sayilar.Add(rnd.Next(-20, 30));
+        }
+
+        Console.WriteLine("Rastgele sayılar: "+ string.Join(",",sayilar));
+
+        var ciftSayilar = sayilar.Where(s => s % 2 == 0).ToList();
+        Console.WriteLine("Çift sayılar: " +string.Join(",",ciftSayilar));
+
+
+        var tekSayilar = sayilar.Where(s => s % 2 != 0).ToList();
+        Console.WriteLine("Tek sayılar: "+string.Join(",",tekSayilar));
+
+
+        var negatifSayilar = sayilar.Where(s => s < 0).ToList();
+        Console.WriteLine("Tek sayılar: "+string.Join(",",negatifSayilar));
+
+
+        var pozitifSayılar = sayilar.Where(s => s > 0).ToList();
+        Console.WriteLine("Pozitif sayılar: "+string.Join(",",pozitifSayılar));
+
+
+        var aralikSayilar = sayilar.Where(s => s > 15 && s < 22).ToList();
+        Console.WriteLine("15 < s < 22 Sayılar: " + string.Join(", ", aralikSayilar));
+
+        // 7. Listedeki sayıların kareleri
+        var karelerListesi = sayilar.Select(s => s * s).ToList();
+        Console.WriteLine("Sayıların Kareleri: " + string.Join(", ", karelerListesi));
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ Patifaky/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace MusicDataLinq
+{
+    public class Singer
+    {
+        public string FullName { get; set; }
+        public string MusicGenre { get; set; }
+        public int DebutYear { get; set; }
+        public double AlbumSales { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Singer> singers = new List<Singer>
+            {
+                new Singer { FullName = "Ajda Pekkan", MusicGenre = "Pop", DebutYear = 1968, AlbumSales = 20 },
+                new Singer { FullName = "Sezen Aksu", MusicGenre = "Türk Halk Müziği / Pop", DebutYear = 1971, AlbumSales = 10 },
+                new Singer { FullName = "Funda Arar", MusicGenre = "Pop", DebutYear = 1999, AlbumSales = 3 },
+                new Singer { FullName = "Sertab Erener", MusicGenre = "Pop", DebutYear = 1994, AlbumSales = 5 },
+                new Singer { FullName = "Sıla", MusicGenre = "Pop", DebutYear = 2009, AlbumSales = 3 },
+                new Singer { FullName = "Serdar Ortaç", MusicGenre = "Pop", DebutYear = 1994, AlbumSales = 10 },
+                new Singer { FullName = "Tarkan", MusicGenre = "Pop", DebutYear = 1992, AlbumSales = 40 },
+                new Singer { FullName = "Hande Yener", MusicGenre = "Pop", DebutYear = 1999, AlbumSales = 7 },
+                new Singer { FullName = "Hadise", MusicGenre = "Pop", DebutYear = 2005, AlbumSales = 5 },
+                new Singer { FullName = "Gülben Ergen", MusicGenre = "Pop / Türk Halk Müziği", DebutYear = 1997, AlbumSales = 10 },
+                new Singer { FullName = "Neşet Ertaş", MusicGenre = "Türk Halk Müziği / Türk Sanat Müziği", DebutYear = 1960, AlbumSales = 2 }
+            };
+
+            var StartWithS = singers
+                .Where(s => s.FullName.StartsWith("S"))
+                .Select(s => s.FullName);
+            Console.WriteLine("S ile başlayan şarkıcılar: ");
+            Console.WriteLine(string.Join(",",StartWithS));
+            Console.WriteLine();
+
+            var above10Million = singers
+               .Where(s => s.AlbumSales > 10)
+               .Select(s => s.FullName);
+            Console.WriteLine("🔸 Albüm satışı 10 milyondan fazla olanlar:");
+            Console.WriteLine(string.Join(", ", above10Million));
+            Console.WriteLine();
+
+            // 3. 2000 öncesi çıkış yapmış ve Pop müzik yapanlar (sıralı)
+            var pre2000Pop = singers
+                .Where(s => s.DebutYear < 2000 && s.MusicGenre.Contains("Pop"))
+                .OrderBy(s => s.DebutYear)
+                .ThenBy(s => s.FullName);
+            Console.WriteLine("🔸 2000 öncesi çıkış yapmış Pop sanatçıları:");
+            foreach (var singer in pre2000Pop)
+            {
+                Console.WriteLine($"{singer.DebutYear} - {singer.FullName}");
+            }
+            Console.WriteLine();
+
+            // 4. En çok albüm satan şarkıcı
+            var topSeller = singers
+                .OrderByDescending(s => s.AlbumSales)
+                .First();
+            Console.WriteLine($"🔸 En çok albüm satan şarkıcı: {topSeller.FullName} ({topSeller.AlbumSales} milyon)");
+            Console.WriteLine();
+
+            // 5. En yeni ve en eski çıkış yapan şarkıcılar
+            var newestSinger = singers.OrderByDescending(s => s.DebutYear).First();
+            var oldestSinger = singers.OrderBy(s => s.DebutYear).First();
+            Console.WriteLine($"🔸 En yeni çıkış yapan şarkıcı: {newestSinger.FullName} ({newestSinger.DebutYear})");
+            Console.WriteLine($"🔸 En eski çıkış yapan şarkıcı: {oldestSinger.FullName} ({oldestSinger.DebutYear})");
+
+
+
+
+        }
+
+
+
+    }
+
+}
